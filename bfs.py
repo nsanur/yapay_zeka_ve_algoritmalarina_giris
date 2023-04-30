fc2bdf1c0054833a2b59d1579ed57000dedb22a3
N <103697226+nsanur@users.noreply.github.com>
2023-04-30 09:51:23 +0300

    Add files via upload

---
+++ bfs.py
@@ -0,0 +1,26 @@
+#yayilim oncelikli arama (bfs)
+grafik = {
+        'A' : ['B', 'C'],
+        'B' : ['D', 'E'],
+        'C' : ['F'],
+        'D' : [],
+        'E' : [],
+        'F' : [],
+
+}
+ziyaret = []
+yigin = []
+
+def bfs(ziyaret, grafik, dugum):
+    ziyaret.append(dugum)
+    yigin.append(dugum)
+
+    while yigin:
+        s = yigin.pop(0)
+        print(s, end = " ")
+
+        for komsu in grafik[s]:
+            if komsu not in ziyaret:
+                yigin.append(komsu)
+
+bfs(ziyaret, grafik, 'A')
